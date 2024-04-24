7a8ccae6190c522205790cd57758161c1e3d5097
Bahodir10 <abdullajon6246@gmail.com>
2024-04-24 09:37:02 +0500

    Task3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task3
+        String[] j = {"cd","ac","dc","ca","zz"};
+        int count = 0;
+        for(int i = 0;i<j.length-1;i++){
+            for(int a = i+1;a<j.length;a++){
+                if(Objects.equals(j[i],reverse(j[a]))){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+    }
+    public static String reverse(String str){
+        StringBuilder newStr = new StringBuilder();
+        for(int i = str.length()-1;i>-1;i--){
+            newStr.append(str.charAt(i));
+        }
+        return new String(newStr);
     }
 }
